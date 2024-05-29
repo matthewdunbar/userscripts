1ea5ed8e97d71167f994ad3960152b22ed208b5c
Matthew Dunbar <matthew.dunbar@lifeway.com>
2024-05-29 09:07:26 -0400

    Set version

--- epublican-view.js
+++ epublican-view.js
@@ -9,6 +9,7 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @run-at       document-idle
 // @grant        none
+// @version      1.0
 // ==/UserScript==
 
 window.ReactNativeWebView = {
