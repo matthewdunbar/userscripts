db32725e83e3380d4e73b8092850121fee5a4418
Matthew Dunbar <matthew.dunbar@lifeway.com>
2024-05-29 08:46:13 -0400

    Add epublican view

---
+++ epublican-view.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Reader Dev Commands
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-17
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @url          https://github.com/matthewdunbar/userscripts/epublican-view.js
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+window.ReactNativeWebView = {
+  postMessage: (msg) => {
+    console.log(`Received message from diode ${msg}`);
+  },
+};
+
+window.sendCommand = (cmd) => {
+  dispatchEvent(
+    new MessageEvent("ReaderCommand", {
+      data: cmd,
+    })
+  );
+};
+
+window.Commands = {
+  setToken: (token) => {
+    return {
+      _type: "SetAccessToken",
+      requestId: "requestId",
+    };
+  },
+  openCSB: {
+    _type: "OpenBook",
+    contentId: "3BAFECA7-66C1-40BC-B6E5-AFDE2D94B527",
+    requestId: "requestId",
+  },
+  navigateToLocation: (location) => {
+    return {
+      _type: "NavigateToLocation",
+      location: location,
+      requestId: "requestId",
+    };
+  },
+};
